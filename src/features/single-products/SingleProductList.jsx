9a79f766200422f0b6115b6a2ccd0c45121980b3
Dzmitry <120422809+DzmitryDM@users.noreply.github.com>
2023-10-27 19:48:31 +0300

    test

--- src/features/single-products/SingleProductList.jsx
+++ src/features/single-products/SingleProductList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSingleProducts } from './useSingleProducts'
 import styles from './../../styles/Products.module.css'
 
@@ -6,6 +6,9 @@ function SingleProductList() {
 	const [images, { status, product, error }] =
 		useSingleProducts()
 	
+		const [currentImage, setCurrentImage] = useState(null)
+
+
 
 
 	return (
@@ -13,18 +16,15 @@ function SingleProductList() {
 			{status === 'received' && (
 				<section className={styles.single_products}>
 					<div className={styles.single_wrapper_img}>
-						<img
-							className={styles.single_image}
-							src={images[0]}
-							alt='imag'
-						/>
+						<img className={styles.single_image} src={currentImage === null ? images[0]:currentImage} alt='imag' />
 						{/* <div style={{ backgroundImage: `url(${currimages})`}}> </div> */}
-						<div className={styles.single_images}>
+						<div className={styles.single_images}>	
 							{images.map((image, i) => (
 								<img
 									key={i}
 									src={image}
 									alt='imag'
+									onClick={() => setCurrentImage(image)}
 								/>
 							))}
 						</div>
